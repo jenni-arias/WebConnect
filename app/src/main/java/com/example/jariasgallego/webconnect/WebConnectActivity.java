73d2b8ba99f21a60f2bf658ff73300e1c750b29e
jenni-arias <arias.jenni@gmail.com>
2017-10-18 20:05:41 +0200

    Creación clase connect

--- app/src/main/java/com/example/jariasgallego/webconnect/WebConnectActivity.java
+++ app/src/main/java/com/example/jariasgallego/webconnect/WebConnectActivity.java
@@ -2,12 +2,33 @@ package com.example.jariasgallego.webconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.R.id.content;
 
 public class WebConnectActivity extends AppCompatActivity {
+
+    TextView content;
+    EditText edit_url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_connect);
+
+        // canviem els atributs amb el onCreate, així quan s'inicia l'app, s'inicien els camps
+       content = (TextView) findViewById(R.id.content);
+       edit_url = (EditText) findViewById(R.id.url);
+    }
+
+    public void connect(View view) {
+
+        String surl = edit_url.getText().toString(); // el text que has posat al edittext
+
+        String cont = WebFetcher.getUrl(surl); // de la classe WebFetcher cridem la funció estàtica getURL
+        content.setText(cont);
+
     }
 }
---
+++ app/src/main/java/com/example/jariasgallego/webconnect/WebFetcher.java
@@ -0,0 +1,49 @@
+package com.example.jariasgallego.webconnect;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by j.arias.gallego on 18/10/2017.
+ */
+
+
+class WebFetcher {
+
+
+        static String getUrl(String s_url) {   // funció que retorna un string
+            // 1.Creem un objecte de tipus URL
+
+            try {
+                URL url = new URL(s_url);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // el que es conecta
+                if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) { // si la conexió és diferent 200 ( està bé) marxem
+                    return "<error: No HTTP_OK>";
+                }
+                // ara venen els bytes per la xarxa
+                InputStream in = conn.getInputStream(); // passa'm els bytes i els vaig enxtraient i els guardo a un array
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                coppyStream(in, out); // funció inventada (alt+Enter --> create method)
+                return out.toString(); // retorno la sortida amb els bytes ja copiats
+            } catch (IOException e) {
+                return "<error IOException>";
+            }
+        }
+
+
+    private static void coppyStream(InputStream in, ByteArrayOutputStream out)
+        //Gestionar els error en vermell: Ens diu que no hem gestionat una excepció, amb això aquesta funció gestiona excepcions
+            throws IOException
+    {
+        // Copiar els bits d'un Steam a un altre
+        byte[] bytes = new byte[1024];
+        int nbytes = in.read(bytes); // llegim els bytes de l'entrada
+        while (nbytes > 0) {
+            out.write(bytes, 0, nbytes);
+            nbytes = in.read(bytes);
+        }
+    }
+}
